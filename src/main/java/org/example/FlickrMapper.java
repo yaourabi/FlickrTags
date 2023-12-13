51a0aa0f2374423fdfeda9bd46bdfe8ec7000dd3
alamiaro <mleo691@gmail.com>
2023-12-13 18:15:36 +0100

    Question 18

--- src/main/java/org/example/FlickrMapper.java
+++ src/main/java/org/example/FlickrMapper.java
@@ -3,31 +3,41 @@ package org.example;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.Mapper;
 
 
+import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.Arrays;
 
-/*public class FlickrMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class FlickrMapper extends Mapper<LongWritable, Text, Text, StringAndInt> {
+
+    private boolean isValidField(String field) {
+        return field != null && !field.isEmpty();
+    }
+
+    private Country getCountry(String longitude, String latitude) {
+        if (isValidField(longitude) && isValidField(latitude)) {
+            return Country.getCountryAt(Double.parseDouble(latitude), Double.parseDouble(longitude));
+        }
+        return null;
+    }
+
 
     @Override
-    protected void map(LongWritable key, Text value, Context context) {
-        String[] parts = value.toString().split("\t");
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] records = value.toString().split("\t");
+        String longitude = records[10];
+        String latitude	 = records[11];
 
-        try {
+        Country country = getCountry(longitude, latitude);
 
-            double latitude = Double.parseDouble(parts[11]);
-            double longitude = Double.parseDouble(parts[10]);
-            Country country = Country.getCountryAt(latitude, longitude);
+        if (country != null) {
+            String tags = URLDecoder.decode(records[8],"UTF-8") + "," + URLDecoder.decode(records[9],"UTF-8");
 
-            if (country != null) {
-                String tags = URLDecoder.decode(parts[8], "UTF-8");
-                for (String tag : tags.split(",")) {
-                    context.write(new Text(country.toString()), new Text(tag));
-                }
-            }
-        } catch (Exception e) {
-            Counter counter = context.getCounter("FlickrPhotoMapper", "Malformatted Lines");
-            counter.increment(1);
+            for (String tag : tags.split(","))
+                context.write(new Text(country.toString()), new StringAndInt(tag));
         }
     }
-}*/
+
+}
